4d11cb010ebddfe0ebc8b2f1a9777707ca35b7bc
Andrea Jemmett <andreajemmett@gmail.com>
2017-02-03 11:57:31 +0100

    style tweaking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   mapClickUrl = 'https://maps.google.com/?q=Palazzo%20Michiel%20Strada%20Nuova,%204391%20Campo%20Santi%20Apostoli%20Venezia';
   staticMapUrl = 'https://maps.googleapis.com/maps/api/staticmap'+
           '?center=Palazzo%20Michiel%20Strada%20Nuova,%204391%20Campo%20Santi%20Apostoli%20Venezia' +
@@ -15,5 +15,14 @@ export class AppComponent {
           '&maptype=roadmap' +
           '&markers=color:red%7CPalazzo%20Michiel%20Strada%20Nuova,%204391%20Campo%20Santi%20Apostoli%20Venezia' +
           '&key=AIzaSyDOtyBGM1ArybPLKhVIcBZzL_TKy2DWNxQ';
+
+  ngOnInit() {
+    let h = parseInt(
+      window.getComputedStyle(document.querySelector('#timeline')).height);
+    let lih = parseInt(
+      window.getComputedStyle(document.querySelector('#timeline li:last-child')).height);
+    let ss = <CSSStyleSheet>document.styleSheets[0];
+    ss.insertRule(`#timeline::before { height: ${h-lih}px !important; }`, 0);
+  }
 
 }

a8f4a87920da12f6064359c290a1b9698263eee5
Andrea Jemmett <andreajemmett@gmail.com>
2017-02-03 14:47:33 +0100

    setup backend

---
+++ back/app/Http/Controllers/PartecipaController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use PHPMailer;
+use Illuminate\Http\Request;
+use Log;
+
+class PartecipaController extends Controller
+{
+
+    public function send_email(Request $request)
+    {
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+        $mail->Host = env('SMTP_HOST');
+        $mail->Username = env('SMTP_USERNAME');
+        $mail->Password = env('SMTP_PASSWORD');
+        $mail->SMTPAuth = true;
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        $mail->Port = (int) env('SMTP_PORT');
+
+        $mail->setFrom(env('PARTECIPA_FROM_ADDRESS'));
+        $mail->addAddress(env('PARTECIPA_TO_ADDRESS'));
+
+        if (empty($request->input('name'))
+            || empty($request->input('surname'))
+            || empty($request->input('address'))
+            || empty($request->input('message')))
+            return response([ 'error' => 'Missing fields' ], 400);
+
+        $mail->Subject = 'Nuovo messaggio da '
+                            . $request->input('name')
+                            . ' ' . $request->input('surname');
+        $mail->Body = 'Nome: ' . $request->input('name') . "\n"
+                    . 'Cognome: ' . $request->input('surname') . "\n"
+                    . 'Email: ' . $request->input('address') . "\n\n\n"
+                    . $request->input('message');
+
+        if(!$mail->send()) {
+            Log::error("Message could not be sent.\nMailer Error: $mail->ErrorInfo");
+            return response([ 'error' => $mail->ErrorInfo ], 500);
+        }
+
+        return response('', 201);
+    }
+
+}
